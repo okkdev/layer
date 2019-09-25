1da974ae3fe2b2b648360444181b4d6c5a428cc7
okkdev <stehlik.jen@gmail.com>
2019-09-25 21:17:27 +0200

    add command line

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,40 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    $
+    <input v-model="command" type="text" @keyup.enter="execute" />
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      command: ''
+    }
+  },
+  methods: {
+    execute(event) {
+      switch (this.command.trim) {
+        case 'help':
+          // code block
+          break
+        case 'whoami':
+          // code block
+          break
+        case '':
+          break
+        default:
+          return (
+            '"' +
+            this.command +
+            '"' +
+            ' is not a valid command. Run help to see valid commands.'
+          )
+      }
+      alert(this.command)
+    }
+  }
+}
 </script>
 
 <style></style>
