f5f033ef3c37abb9acf3b517043b0e480b1c9eda
okkdev <stehlik.jen@gmail.com>
2019-11-19 15:34:42 +0100

    add background images

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="container mx-auto h-screen">
-    <Commandline></Commandline>
+  <div class="bg-cover" :style="'background-image: url(' + bgImage + ')'">
+    <div class="container mx-auto h-screen">
+      <Commandline></Commandline>
+    </div>
   </div>
 </template>
 
@@ -10,8 +12,22 @@ import Commandline from '~/components/Commandline'
 export default {
   components: {
     Commandline
+  },
+  data: () => ({
+    bgImage: '',
+    imageNo: 8
+  }),
+  mounted() {
+    this.bgImage = 'img/' + Math.floor(Math.random() * this.imageNo) + '.gif'
   }
 }
 </script>
 
-<style></style>
+<style>
+body {
+  font-family: 'Overpass Mono', 'Courier New', Courier, monospace;
+  font-size: 1.2em;
+  color: beige;
+  background-color: black;
+}
+</style>
