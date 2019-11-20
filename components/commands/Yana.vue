738ac0f0300ae9f56ddec2e3de1948b9ee755388
jen.stehlik <jen.stehlik@kallysoft.ch>
2019-11-20 13:30:59 +0100

    cleanup

--- components/commands/Yana.vue
+++ components/commands/Yana.vue
@@ -8,9 +8,6 @@
       viewBox="0 0 48.000000 48.000000"
       preserveAspectRatio="xMidYMid meet"
     >
-      <metadata>
-        Created by potrace 1.15, written by Peter Selinger 2001-2017
-      </metadata>
       <g
         transform="translate(0.000000,48.000000) scale(0.100000,-0.100000)"
         fill="#f76f88"
