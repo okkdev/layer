afda30b5e32ad656d017a3c76045d3879cffd859
okkdev <stehlik.jen@gmail.com>
2019-11-20 21:41:38 +0100

    add clear to help

--- components/commands/Help.vue
+++ components/commands/Help.vue
@@ -3,10 +3,12 @@
     <div class="mr-3 text-orange-200">
       <div>help</div>
       <div>whoami</div>
+      <div>clear</div>
     </div>
     <div>
       <div>- see all possible commands</div>
       <div>- find out about me</div>
+      <div>- clear command history</div>
     </div>
   </div>
 </template>
