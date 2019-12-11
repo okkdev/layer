a143b670c53c73a24dff6cfdb8c2cd7638243985
jen.stehlik <jen.stehlik@kallysoft.ch>
2019-12-11 15:32:30 +0100

    update help

--- components/commands/Help.vue
+++ components/commands/Help.vue
@@ -1,16 +1,12 @@
 <template>
-  <div class="text-m flex">
-    <div class="mr-3 text-orange-200">
-      <div>help</div>
-      <div>whoami</div>
-      <div>background</div>
-      <div>clear</div>
-    </div>
-    <div>
-      <div>- see all possible commands</div>
-      <div>- info about me</div>
-      <div>- change the background image</div>
-      <div>- clear command history</div>
-    </div>
+  <div class="text-m">
+    <div class="command">help</div>
+    <div class="description">see all possible commands</div>
+    <div class="command">whoami</div>
+    <div class="description">info about me</div>
+    <div class="command">background, bg</div>
+    <div class="description">change the background image</div>
+    <div class="command">clear</div>
+    <div class="description">clear command history</div>
   </div>
 </template>
