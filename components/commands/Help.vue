9a4f0db44ea9d5d4a560b38974cab06900932d7e
okkdev <stehlik.jen@gmail.com>
2019-12-12 17:30:17 +0100

    update help

--- components/commands/Help.vue
+++ components/commands/Help.vue
@@ -7,6 +7,8 @@
       change the background image <br />
       run "background help" for more info
     </div>
+    <div class="command">[mathematical expression]</div>
+    <div class="description">calculates it</div>
     <div class="command">clear</div>
     <div class="description">clear command history</div>
   </div>
